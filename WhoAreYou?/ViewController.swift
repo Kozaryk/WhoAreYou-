bfd2fab5e6d324bc9eb8de33f6db8f9c9586aa9e
Igor Kozar <kozaryk99@gmail.com>
2021-06-01 19:35:59 +0300

    Initial Commit

---
+++ WhoAreYou?/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WhoAreYou?
+//
+//  Created by Козар Ігор on 01.06.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
